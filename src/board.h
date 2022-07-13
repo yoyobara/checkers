b5fd19edcd52237f4fb68ad26a1ee1dca267a23b
yoyobara <yotambarak6@gmail.com>
2022-07-14 01:19:31 +0300

    added some docstrings to board.h

--- src/board.h
+++ src/board.h
@@ -31,16 +31,42 @@ const char COLUMNS = 4;
 class Board {
 	
 	public:
+
+		/*
+		 initializes board 
+		*/
 		Board();
 
+		/*
+		 get a piece on the board by spot 
+		*/
 		PieceType get_piece(Spot s);
+
+		/*
+		 gets an adjacent piece on a real checkers board 
+		*/
 		Spot get_adjacent(Spot s, AdjIndex idx);
+		
+		/*
+		 get_adjacent + get_piece 
+		*/
 		PieceType get_adjacent_piece(Spot s, AdjIndex idx);
 
+		/*
+		 resets the position of the pieces to the default 
+		*/
 		void reset_pieces();
+
+		/*
+		 prints the board to the console 
+		*/
 		void print();
 
 	private:
+
+		/*
+		 representing the 8x8 checkers board using a 8x4 array. 
+		*/
 		std::array<std::array<PieceType, COLUMNS>, ROWS> arr;
 };
 
